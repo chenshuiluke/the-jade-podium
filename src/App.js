b9cbb475d931b8c4cbc7bad79470be5c1946afd7
Luke Chen Shui <chenshuiluke@gmail.com>
2018-07-13 22:47:36 -0500

    Imported the Bulma CSS Framework

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Columns } from 'react-bulma-components';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Columns>
+            <Columns.Column>
+              First Column
+            </Columns.Column>
+            <Columns.Column>
+              Second Column
+            </Columns.Column>
+            <Columns.Column>
+              Third Column
+            </Columns.Column>
+            <Columns.Column>
+              Fourth Column
+            </Columns.Column>
+          </Columns>
       </div>
     );
   }
