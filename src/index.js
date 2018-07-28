a80207052dac94ca563b756fca72320d3eff3d3e
Luke Chen Shui <chenshuiluke@gmail.com>
2018-07-28 03:26:32 -0500

    Fixed the `npm run build` error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
-require('dotenv').config();
 ReactDOM.render(
 <Router>
   <App />
