b0de2df27b091da11c4d41cfd1d2c90e915d8a63
Luke Chen Shui <chenshuiluke@gmail.com>
2018-07-28 04:09:56 -0500

    Added sequalize ORM alongside a basic User model and migration.

---
+++ server/models/user.js
@@ -0,0 +1,8 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {}, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
